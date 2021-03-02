2dd0b441f92425ac7a7d48c4d4ea7f0f92bf487b
Francesca Santoriello <f.santoriello@hotmail.com>
2021-03-02 16:10:24 +0100

    code category#new & category#create actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,25 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @user = current_user
+    @category = Category.new(category_params)
+    @category.user = @user
+    if @category.save
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :goal_per_month)
+  end
 end
