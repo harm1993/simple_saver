1491aa1e4a8623c11b439d4fc03a92fb11354624
Francesca Santoriello <f.santoriello@hotmail.com>
2021-03-02 16:34:17 +0100

    code categories#destroy action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,6 +27,12 @@ class CategoriesController < ApplicationController
     @category.update(category_params)
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to root_path
+  end
+
   private
 
   def category_params
