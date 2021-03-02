da2e0b4391dce90a1bb3445d674cf0085ce14d89
Thomas de Jong <thomasalexanderdejong@gmail.com>
2021-03-02 11:58:55 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :categories do
+    resources :expenses, only: [ :index, :new, :create ]
+  end
+  resources :expenses, only: [ :edit, :destroy ]
+  resources :goals, only: [ :show, :new, :create, :edit, :update, :destroy ]
 end
