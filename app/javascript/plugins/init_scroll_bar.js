01f6f584b92a9884759ea3f8028d5088924a21d3
Francesca Santoriello <f.santoriello@hotmail.com>
2021-03-04 17:42:05 +0100

    fix bug

--- app/javascript/plugins/init_scroll_bar.js
+++ app/javascript/plugins/init_scroll_bar.js
@@ -1,7 +1,59 @@
 import { MiniBar } from "minibarjs";
 
 const initScrollBar = () => {
-  new MiniBar('.expenses');
+  new MiniBar('.expenses', ,{
+
+    // or progress
+    barType: "default",
+
+    // min size
+    minBarSize: 10,
+
+    // always shows scrollbars
+    alwaysShowBars: false,
+
+    // enables horizontal/vertical scrollbars
+    scrollX: true,
+    scrollY: true,
+
+    // shows nav buttons
+    navButtons: false,
+
+    // scroll amount in pixels
+    scrollAmount: 10,
+    
+    // MutationObserver API
+    mutationObserver: {
+      attributes: false,
+      childList: true,
+      subtree: true
+    },
+
+    // default classes
+    classes: {
+      container: "mb-container",
+      content: "mb-content",
+      track: "mb-track",
+      bar: "mb-bar",
+      visible: "mb-visible",
+      progress: "mb-progress",
+      hover: "mb-hover",
+      scrolling: "mb-scrolling",
+      textarea: "mb-textarea",
+      wrapper: "mb-wrapper",
+      nav: "mb-nav",
+      btn: "mb-button",
+      btns: "mb-buttons",
+      increase: "mb-increase",
+      decrease: "mb-decrease",
+      item: "mb-item",
+      itemVisible: "mb-item-visible",
+      itemPartial: "mb-item-partial",
+      itemHidden: "mb-item-hidden"
+    }
+
+});
+);
 }
 
 export { initScrollBar }
