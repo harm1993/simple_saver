860a906e7e58c06324431254d0dab75636f4e2f9
Harm Binnenkade <harmbinnenkade@gmail.com>
2021-03-05 12:42:50 +0100

    arrow debug plus link visiblity fixed

--- app/javascript/plugins/init_scroll_bar.js
+++ app/javascript/plugins/init_scroll_bar.js
@@ -1,51 +1,53 @@
 import { MiniBar } from "minibarjs";
 
 const initScrollBar = () => {
-  new MiniBar('.exp-container', {
-    // or progress
-    barType: "default",
-    // min size
-    minBarSize: 10,
-    // always shows scrollbars
-    alwaysShowBars: true,
-    // enables horizontal/vertical scrollbars
-    scrollX: true,
-    scrollY: true,
-    // shows nav buttons
-    navButtons: true,
-    // scroll amount in pixels
-    scrollAmount: 10,
-    // MutationObserver API
-    mutationObserver: {
-      attributes: false,
-      childList: true,
-      subtree: true
-    },
-    // default classes
-    classes: {
-      container: "mb-container",
-      content: "mb-content",
-      track: "mb-track",
-      bar: "mb-bar",
-      visible: "mb-visible",
-      progress: "mb-progress",
-      hover: "mb-hover",
-      scrolling: "mb-scrolling",
-      textarea: "mb-textarea",
-      wrapper: "mb-wrapper",
-      nav: "mb-nav",
-      btn: "mb-button",
-      btns: "mb-buttons",
-      increase: "mb-increase",
-      decrease: "mb-decrease",
-      item: "mb-item",
-      itemVisible: "mb-item-visible",
-      itemPartial: "mb-item-partial",
-      itemHidden: "mb-item-hidden"
-    }
+  if (document.querySelector('.exp-container')) {
+    new MiniBar('.exp-container', {
+      // or progress
+      barType: "default",
+      // min size
+      minBarSize: 10,
+      // always shows scrollbars
+      alwaysShowBars: true,
+      // enables horizontal/vertical scrollbars
+      scrollX: true,
+      scrollY: true,
+      // shows nav buttons
+      navButtons: true,
+      // scroll amount in pixels
+      scrollAmount: 10,
+      // MutationObserver API
+      mutationObserver: {
+        attributes: false,
+        childList: true,
+        subtree: true
+      },
+      // default classes
+      classes: {
+        container: "mb-container",
+        content: "mb-content",
+        track: "mb-track",
+        bar: "mb-bar",
+        visible: "mb-visible",
+        progress: "mb-progress",
+        hover: "mb-hover",
+        scrolling: "mb-scrolling",
+        textarea: "mb-textarea",
+        wrapper: "mb-wrapper",
+        nav: "mb-nav",
+        btn: "mb-button",
+        btns: "mb-buttons",
+        increase: "mb-increase",
+        decrease: "mb-decrease",
+        item: "mb-item",
+        itemVisible: "mb-item-visible",
+        itemPartial: "mb-item-partial",
+        itemHidden: "mb-item-hidden"
+      }
 
-});
-}
+    });
+  }
+};
 
 export { initScrollBar }
 
