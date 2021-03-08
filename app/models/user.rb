df5544dd23c60006bd29077a9e2ac7345256a336
Francesca Santoriello <f.santoriello@hotmail.com>
2021-03-08 13:59:09 +0100

    code user#overall_progress & user#saving_potential

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,48 @@ class User < ApplicationRecord
     goals.where(completed: true)
   end
 
+  def overall_progress
+    categories = self.categories
+    percentages = []
+    categories.each do |category|
+      percentages << category.spent_percentage
+    end
+    (percentages.sum / percentages.count * 100).to_i
+  end
+
+  def saving_potential
+    categories = self.categories
+    unspent = []
+    categories.each do |category|
+      unspent << category.goal_per_month - category.tot_month
+    end
+    unspent.sum.to_i
+  end
+
 #  validates :first_name, presence: true
 #  validates :date_of_birth, presence: true
 end
+
+
+
+
+# TODO #
+
+# #Overall progress
+#   > average of percentages of all categories 
+#     (per month)
+#
+#   categories = current_user.categories
+#   percentages [] = 
+#   > categories.each > category.spent_percentage > push to array
+#   > percentages.sum / percentages.count
+
+
+# #Saving potential
+#   > sum of the unspended amount from all categories
+#     (per month)
+#   
+#   categories = current_user.categories
+#   unspent [] = 
+#   > categories.each > category.goal_per_month - category.tot_month > push to array
+#   > unspended.sum 
