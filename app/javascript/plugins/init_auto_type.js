330cc04a046d006374866d95b0323aeda3745147
Francesca Santoriello <f.santoriello@hotmail.com>
2021-03-09 14:07:25 +0100

    add autotype animation to h2

--- app/javascript/plugins/init_auto_type.js
+++ app/javascript/plugins/init_auto_type.js
@@ -1,8 +1,23 @@
-import { init } from 'ityped'
+import TypeIt from "typeit";
 
 const initAutoType = () => { 
-  const oneElement = document.querySelector('.ityped-cursor')
-  init(oneElement, { showCursor: false, strings: ['Nice', 'One' ] });
+  new TypeIt("#type-1", {
+    strings: "1. Set categories",
+    speed: 75,
+    waitUntilVisible: true
+  }).go();
+
+  new TypeIt("#type-2", {
+    strings: "2. Set your goal",
+    speed: 75,
+    waitUntilVisible: true
+  }).go();
+
+  new TypeIt("#type-hs", {
+    strings: "Happy saving!",
+    speed: 75,
+    waitUntilVisible: true
+  }).go();
 }
 
 export { initAutoType }
