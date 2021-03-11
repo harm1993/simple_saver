0bf410ca4b2a068c1d8fffa861b453ae0f868591
Harm Binnenkade <harmbinnenkade@gmail.com>
2021-03-11 12:51:34 +0100

    meta tags setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # app/controllers/application_controller.rb
+
+  def default_url_options
+    { host: ENV["www.simplesaver.online"] || "localhost:3000" }
+  end
 end
