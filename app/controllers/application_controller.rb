cf39505a471d1a983fa30b87a2d53db4e01b55b2
Harm Binnenkade <harmbinnenkade@gmail.com>
2021-03-11 16:37:42 +0100

    end added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :avatar, :email, :password, :current_password)}
-  # app/controllers/application_controller.rb
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :avatar, :email, :password, :current_password)
+    end
+    # app/controllers/application_controller.rb
+  end
 
   def default_url_options
     { host: ENV["www.simplesaver.online"] || "localhost:3000" }
