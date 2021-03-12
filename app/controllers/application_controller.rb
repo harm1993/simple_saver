013d7be135a99ebf0421581b53281dd7395f3f75
Harm Binnenkade <harmbinnenkade@gmail.com>
2021-03-12 10:57:28 +0100

    getting further in checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-  
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -11,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["www.simplesaver.online"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
